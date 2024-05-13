8a77afeba99e958f224bd43bb585118d5da3da77
Eric Santos <118479896+ericshantos@users.noreply.github.com>
2024-05-12 21:03:04 -0300

    styles: Atualização de versão

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="assistente-fotografica",
-    version="1.0.0",
+    version="1.1.1",
     packages=find_packages(),
     install_requires=["src", "selecionar_CR2", "config_imagem"],
     author="Eric dos Santos",
